85e0c8470c70859b655e9a32e4383be84689d75a
ActiveCurrent <69092634+ActiveCurrent@users.noreply.github.com>
2023-04-14 12:06:24 +0100

    Add files via upload

---
+++ Colonymod1/Colonymod1/publicRail.cs
@@ -0,0 +1,35 @@
+﻿using Jobs;
+using Jobs.Implementations.Construction;
+using ModLoaderInterfaces;
+using NetworkUI;
+using NetworkUI.AreaJobs;
+using NetworkUI.Items;
+using Pipliz;
+using Science;
+using Shared;
+using System;
+using System.Reflection;
+using static Players;
+using AI;
+using BlockEntities;
+using NPC;
+using Pipliz.Collections.Native;
+using Recipes;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Assertions;
+using BlockEntities.Helpers;
+using Transport.Elevator;
+using static ItemTypesServer;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace james.publicRail
+{
+    [ModLoader.ModManager]
+    public class publicRail
+    {
+    }
+}
